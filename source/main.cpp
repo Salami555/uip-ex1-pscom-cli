7a27dc411866d88a59cd72b2bc2db9a6dc218dd2
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2018-11-13 00:54:14 +0100

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,46 @@
+
+#include <pscom.h>
+
+#include <QCoreApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QRegExp>
+
+#include "verbosity.h"
+
+
+/* PLEASE NOTE
+ * - be carful using the pscom library, it may irreversible delete actual data if used without care.
+ * - try to rely on pscom library exclusivel in order to fulfill the user stories / tasks.
+ * - use qt only to drive the library and control command language aspects and CLI related features, e.g.,
+ * progress, logging, verbosity, undo, help, etc.
+ * - prefer qDebug, qInfo, qWarning, qCritical, and qFatal for verbosity and console output.
+ */
+
+
+/* TODOs
+ * - adjust the application version using QCoreApplication::setApplicationVersion before creating an application object.
+ * - make yourself familiar with semantic versioning, e.g., here https://semver.org/, if you like, since it can useful
+ * for CLIs. For example, it allows to detach the version number of the CLI from the version of the library used and may
+ * retain major version bumps (same CLI for different capabilities or naming of the used library).
+ */
+
+
+int main(int argc, char *argv[])
+{
+    qInstallMessageHandler(VerbosityHandler);
+
+    // Setting the application name is not required, since, if not set, it defaults to the executable name.
+    // QCoreApplication::setApplicationName("pscom-cli");
+
+    QCoreApplication::setApplicationVersion("0.0.0");
+
+    QCoreApplication a(argc, argv);
+
+
+    /* ToDo - have fun! */
+
+
+    return a.exec();
+}
