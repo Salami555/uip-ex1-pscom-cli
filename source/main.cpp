89b2b9e1f16fe3bb040aa60b6d8ca87885554ddc
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2018-11-13 01:38:32 +0100

    add application version support, add some library bindings

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include <QCommandLineParser>
 #include <QDebug>
 #include <QRegExp>
+#include <QVersionNumber>
 
 #include "verbosity.h"
 
@@ -18,29 +19,70 @@
  * - prefer qDebug, qInfo, qWarning, qCritical, and qFatal for verbosity and console output.
  */
 
+using namespace std;
 
-/* TODOs
- * - adjust the application version using QCoreApplication::setApplicationVersion before creating an application object.
- * - make yourself familiar with semantic versioning, e.g., here https://semver.org/, if you like, since it can useful
- * for CLIs. For example, it allows to detach the version number of the CLI from the version of the library used and may
- * retain major version bumps (same CLI for different capabilities or naming of the used library).
- */
+namespace lib_utils {
 
+    namespace io_ops {
+        QStringList supportedFormats() {
+            return pscom::sf();
+        }
+        bool isPathExistingDirectory(const QString & path) {
+            return pscom::de(path);
+        }
+        bool isPathExistingFile(const QString & path) {
+            return pscom::fe(path);
+        }
+        bool isPathExisting(const QString & path) {
+            return !pscom::ne(path);
+        }
+        QString fileExtension(const QString & path) {
+            if(!isPathExistingFile(path)) {
+                throw "";
+            }
+            return pscom::fs(path);
+        }
+        QString replaceFileExtensionIfSupported(const QString & path, const QString & extension) {
+            if(!supportedFormats().contains(extension)) {
+                throw "";
+            }
+            return pscom::cs(path, extension);
+        }
+
+    }
+
+}
+
+static const auto VERSION = QVersionNumber(1, 0, 0);
+
+void initApplication() {
+    // Setting the application name is not required, since, if not set, it defaults to the executable name.
+    QCoreApplication::setApplicationName("pscom-cli");
+    QCoreApplication::setApplicationVersion(VERSION.toString());
+    
+    // Read pscom version with format "%appName% version %appVersion% | pscom-%pscomVersion% qt-%qtVersion%"
+    auto libVersion = pscom::vi();
+    auto libVersionMessage = libVersion.right(libVersion.length() - libVersion.indexOf("pscom-", 1));
+    
+    QString versionString("%1 v%2 | %3");
+    QCoreApplication::setApplicationVersion(versionString
+        .arg(QCoreApplication::applicationName())
+        .arg(QCoreApplication::applicationVersion())
+        .arg(libVersionMessage));
+}
 
 int main(int argc, char *argv[])
 {
     qInstallMessageHandler(VerbosityHandler);
-
-    // Setting the application name is not required, since, if not set, it defaults to the executable name.
-    // QCoreApplication::setApplicationName("pscom-cli");
-
-    QCoreApplication::setApplicationVersion("0.0.0");
-
-    QCoreApplication a(argc, argv);
-
+    QCoreApplication app(argc, argv);
+    initApplication();
 
     /* ToDo - have fun! */
+    
+    // qDebug() << app.applicationDirPath();
 
+    qDebug() << app.applicationName();
+    qDebug() << app.applicationVersion();
 
-    return a.exec();
+    return app.exec();
 }
